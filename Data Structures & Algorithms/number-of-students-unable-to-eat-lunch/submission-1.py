22e162e891057305bc62f23cecfe5591339ec098
Nicholas Ung <nicholasung22@gmail.com>
2026-08-27 16:29:49 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        s_freqs = defaultdict(int)
+        for student in students:
+            s_freqs[student] += 1
+        
+        for sandwich in sandwiches:
+            if s_freqs[sandwich] == 0:
+                return s_freqs[ 1 if sandwich == 0 else 0]
+            s_freqs[sandwich] -= 1
+
+        return 0
