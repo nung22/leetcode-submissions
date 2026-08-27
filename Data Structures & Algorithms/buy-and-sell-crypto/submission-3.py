84bc9879f0f9e61ef8f626461403d81376525d4a
Nicholas Ung <nicholasung22@gmail.com>
2026-08-27 10:09:01 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        slow, fast = 0, 1
+        max_profit = 0
+
+        while fast < len(prices):
+            current_profit = prices[fast] - prices[slow]
+            max_profit = max(max_profit, current_profit)
+            if prices[fast] < prices[slow]:
+                slow = fast
+            fast += 1
+        
+        return max_profit
